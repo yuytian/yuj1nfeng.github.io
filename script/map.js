e3f509a98e8bd1d9705f5f0adaf0e479e14bfb17
yujf <296519653@qq.com>
2026-05-10 17:42:37 +0800

    das

--- script/map.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('applyForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    document.getElementById('successModal').classList.remove('hidden');
-});
-
-function closeModal() {
-    document.getElementById('successModal').classList.add('hidden');
-}
